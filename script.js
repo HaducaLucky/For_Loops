8fb36b08b00f2ba689919e687fc8b661de570da7
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-03-28 18:19:56 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//* for loop = repear some code a LIMITED amount of times
+
+// for(let i = 0; i <= 2; i++){
+//     console.log("Hello");
+// }
+
+// for(let i = 10; i > 0; i-=2){
+//     console.log(i)
+// }
+
+// console.log("HAPPY NEW YEAR")
+
+
+//! continue;
+// for(let i = 1; i <= 20; i++){
+
+//     if(i == 13){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+//! break
+for(let i = 1; i <= 20; i++){
+
+    if(i == 13){
+        break;
+    }
+    else{
+        console.log(i);
+    }
+}
